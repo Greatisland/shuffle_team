7f2d8455fdf801eb635555c7d46f765a4f798b48
Hyeon JIN <dontsendloveletters@gmail.com>
2023-04-20 07:21:36 +0000

    몰라

--- main.js
+++ main.js
@@ -73,4 +73,52 @@ const randomSadari = (hard, soft) => {
   team3Text.textContent = '3팀:' + team3
 }
 
- document.querySelector('.button').addEventListener('click',() => randomSadari(gyungjuHard,gyungjuSoft))
+const randomSadari2 = (hard, soft) => {
+  let [team1, team2, team3] = [shuffleArray(hard.slice()),[],shuffleArray(soft.slice())]
+  team2 = team1.splice(0,5)
+  let total = [team1, team2, team3]
+  const requiredMembers = ['석준', '석한', '민정', '재형', '효정', '현진']
+  
+  total.forEach((team, i, total) => {
+    const checkCouple = (team) => team.includes('재형') && team.includes('효정')
+    let lest = total.filter((team, index) => index !== i)
+
+    if(checkCouple(team)){
+      const other = team.splice(team.findIndex(human => human === '재형'), 1)[0]
+      lest.sort((a,b) => a.length - b.length)
+      lest[0].push(other)
+      team.push(lest[0].shift())
+    }
+
+    const checkManage = (team) => requiredMembers.some(member => team.includes(member))
+    if(!checkManage(team)){
+      lest.forEach((lestTeam) => {
+        requiredMembers.some(member => {
+          const manageIndex = lestTeam.findIndex(human => human === member)
+          if(manageIndex !== -1){
+            team.push(lestTeam.splice(manageIndex, 1,)[0])
+            return true
+          }
+        })
+
+        const firstNonRequiredIndex = team.findIndex(member => !requiredMembers.includes(member))
+        if (firstNonRequiredIndex !== -1) {
+          const nonRequiredMember = team.splice(firstNonRequiredIndex, 1)[0]
+          lestTeam.push(nonRequiredMember)
+        }
+
+      })
+    }
+  })
+
+  
+  const team1Text = document.querySelector('.team1')
+  const team2Text = document.querySelector('.team2')
+  const team3Text = document.querySelector('.team3')
+
+  team1Text.textContent = '1팀:' + team1
+  team2Text.textContent = '2팀:' + team2
+  team3Text.textContent = '3팀:' + team3
+}
+
+document.querySelector('.button').addEventListener('click',() => randomSadari2(gyungjuHard,gyungjuSoft))
